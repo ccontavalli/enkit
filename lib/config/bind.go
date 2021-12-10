e0ebb41d74014181561170c158291d53f8470055
ccontavalli <ccontavalli@gmail.com>
2021-12-09 21:21:08 -0800

    lib/config: create bound config abstraction.

---
+++ lib/config/bind.go
@@ -0,0 +1,24 @@
+package config
+
+type Binding interface {
+	Marshal(value interface{}) error
+        Unmarshal(value interface{}) error
+}
+
+type StoreBinding struct {
+	store Store
+	key string
+}
+
+func Bind(store Store, key string) *StoreBinding {
+	return &StoreBinding{store: store, key: key}
+}
+
+func (b *StoreBinding) Marshal(value interface{}) error {
+	return b.store.Marshal(b.key, value)
+}
+
+func (b *StoreBinding) Unmarshal(value interface{}) error {
+	_, err := b.store.Unmarshal(b.key, value)
+	return err
+}
