8e6e9e0cab0ecabb7dcedf46313943a97b2db6f0
Carlo Contavalli <ccontavalli@gmail.com>
2025-11-27 08:39:11 -0800

    lib/khptt/krequestlog: minor logging improvements.

--- lib/khttp/krequestlog/http.go
+++ lib/khttp/krequestlog/http.go
@@ -3,35 +3,38 @@ package krequestlog
 import (
 	"net/http"
 	"time"
+
+	"github.com/ccontavalli/enkit/lib/khttp"
 )
 
 // NewHandler returns a new http.Handler that logs requests.
 func NewHandler(next http.Handler, mods ...Modifier) http.Handler {
 	opts := NewOptions(mods...)
-	
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		path := r.URL.Path
 		method := r.Method
-		
+		origin := khttp.ClientOrigin(r)
+
 		if opts.LogStart {
-			opts.Printer("HTTP START method=%s path=%s", method, path)
+			opts.Printer("HTTP START origin=%s method=%s path=%s origin=%s", origin, method, path)
 		}
-		
+
 		sw := &statusWriter{ResponseWriter: w}
 		next.ServeHTTP(sw, r)
-		
+
 		if opts.LogEnd {
 			duration := time.Since(start)
 			status := sw.status
 			if status == 0 {
 				status = 200
 			}
-			
+
 			if opts.LogFormat == "apache" {
 				// minimal apache combined style
-				opts.Printer("%s - - [%s] \"%s %s %s\" %d %d \"%s\" \"%s\" %v", 
-					r.RemoteAddr, 
+				opts.Printer("%s - - [%s] \"%s %s %s\" %d %d \"%s\" \"%s\" %v",
+					origin,
 					start.Format("02/Jan/2006:15:04:05 -0700"),
 					method, r.URL.RequestURI(), r.Proto,
 					status, sw.length,
@@ -39,7 +42,7 @@ func NewHandler(next http.Handler, mods ...Modifier) http.Handler {
 					duration,
 				)
 			} else {
-				opts.Printer("HTTP END method=%s path=%s status=%d size=%d duration=%v", method, path, status, sw.length, duration)
+				opts.Printer("HTTP END origin=%s method=%s path=%s status=%d size=%d duration=%v", origin, method, path, status, sw.length, duration)
 			}
 		}
 	})
